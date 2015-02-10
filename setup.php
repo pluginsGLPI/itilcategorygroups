5227bf38645d593f79985c8a384eac6d31a8ca45
Emmanuel Haguet <ehaguet@teclib.com>
2015-02-10 16:14:48 +0100

    Cleanup about menus

--- setup.php
+++ setup.php
@@ -42,18 +42,8 @@ function plugin_init_itilcategorygroups() {
                             array('addtabon' => 'Group'));
 
       if (Session::haveRight('config', READ)) {
-
+         // add to 'Tools' menu :
          $PLUGIN_HOOKS["menu_toadd"]['itilcategorygroups'] = array('tools' => 'PluginItilcategorygroupsMenu');
-
-         /* old */ 
-         //$PLUGIN_HOOKS['submenu_entry']['itilcategorygroups']['options']['PluginItilcategorygroupsCategory']['title']
-         //   = __('Link ItilCategory - Groups', 'itilcategorygroups');
-
-         $PLUGIN_HOOKS['submenu_entry']['itilcategorygroups']['options']['PluginItilcategorygroupsCategory']['page']
-            = '/plugins/itilcategorygroups/front/category.php';
-
-         $PLUGIN_HOOKS['submenu_entry']['itilcategorygroups']['options']['PluginItilcategorygroupsCategory']['links']['search']
-            = '/plugins/itilcategorygroups/front/category.php';
 
          // other hook :
          $PLUGIN_HOOKS['pre_item_update']['itilcategorygroups'] = array('Group' => 'plugin_pre_item_update_itilcategorygroups');
