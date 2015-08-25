458001cdac55a1cee8c29ef7f5d4222e3c95aa63
Alexandre Delaunay <adelaunay@teclib.com>
2015-08-25 17:20:55 +0200

    setup for 0.90

--- setup.php
+++ setup.php
@@ -61,14 +61,14 @@ function plugin_init_itilcategorygroups() {
 // Get the name and the version of the plugin - Needed
 function plugin_version_itilcategorygroups() {
    return array('name'           => __('ItilCategory Groups', 'itilcategorygroups'),
-                'version'        => '0.85+1.0',
+                'version'        => '0.90+1.0',
                 'author'         => "<a href='http://www.teclib.com'>TECLIB'</a>",
                 'homepage'       => 'http://www.teclib.com');
 }
 
 // Optional : check prerequisites before install : may print errors or add to message after redirect
 function plugin_itilcategorygroups_check_prerequisites() {
-   if (version_compare(GLPI_VERSION,'0.85', 'lt') || version_compare(GLPI_VERSION,'0.86', 'ge')) {
+   if (version_compare(GLPI_VERSION,'0.85', 'lt')) {
       echo "This plugin requires GLPI 0.85";
       return false;
    }
