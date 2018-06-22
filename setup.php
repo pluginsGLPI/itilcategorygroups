59dee646018d1efe0b41a17ec0a643bf691f13cc
Cédric Anne <cedric.anne@gmail.com>
2018-06-22 16:55:48 +0200

    Add 2.1.0 version definition

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  @since     2009
  ---------------------------------------------------------------------- */
 
-define ('PLUGIN_ITILCATEGORYGROUPS_VERSION', '2.0.2');
+define ('PLUGIN_ITILCATEGORYGROUPS_VERSION', '2.1.0');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_ITILCATEGORYGROUPS_MIN_GLPI", "9.3");
