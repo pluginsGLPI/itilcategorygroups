0ccece6ba82bacb583960bb47d67d2036cf3a48e
guillaume <gcassou@sii.fr>
2019-03-01 16:24:44 +0100

    Restrict groups in dropdown

--- ajax/group_values.php
+++ ajax/group_values.php
@@ -1,31 +1,38 @@
 <?php
 $AJAX_INCLUDE = 1;
 
-include ("../../../inc/includes.php");
+include "../../../inc/includes.php";
 
 header("Content-Type: text/html; charset=UTF-8");
 Html::header_nocache();
 Session::checkLoginUser();
 
-if (! isset($_REQUEST['itilcategories_id'])) {
-   exit;
+if (!isset($_REQUEST['itilcategories_id'])) {
+  exit;
 }
 
 $ticket_id = (isset($_REQUEST['ticket_id'])) ? $_REQUEST['ticket_id'] : 0;
 
-$condition = PluginItilcategorygroupsCategory::getSQLCondition(
-   intval($ticket_id),
-   intval($_REQUEST['itilcategories_id']),
-   $_REQUEST['type']
+$canApplyFilter = PluginItilcategorygroupsCategory::canApplyFilter(
+  intval($_REQUEST['itilcategories_id'])
 );
-if (empty($condition)) {
-   $condition = [
-      'glpi_groups.is_assign' => 1,
-   ] + getEntitiesRestrictCriteria("", "entities_id", $_SESSION['glpiactive_entity'], 1);
+
+$condition = PluginItilcategorygroupsCategory::getSQLCondition(
+  intval($ticket_id),
+  intval($_REQUEST['itilcategories_id']),
+  $_REQUEST['type']
+);
+
+if (!$canApplyFilter && empty($condition)) {
+  $condition = [
+    'glpi_groups.is_assign' => 1,
+  ] + getEntitiesRestrictCriteria("", "entities_id", $_SESSION['glpiactive_entity'], 1);
 }
 
-$_POST['display_emptychoice'] = true;
-$_POST['itemtype']            = 'Group';
-$_POST['condition']           = Dropdown::addNewCondition($condition);
+if (!empty($condition)) {
+  $_POST['display_emptychoice'] = true;
+  $_POST['itemtype'] = 'Group';
+  $_POST['condition'] = Dropdown::addNewCondition($condition);
 
-require ("../../../ajax/getDropdownValue.php");
+  require "../../../ajax/getDropdownValue.php";
+}
