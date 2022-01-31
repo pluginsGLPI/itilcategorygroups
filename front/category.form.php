ff5dbd3469ebc5c34482c99fd5e14a4a51e06175
Cédric Anne <cedric.anne@gmail.com>
2022-01-31 14:31:45 +0100

    Fix licence headers

--- front/category.form.php
+++ front/category.form.php
@@ -1,32 +1,32 @@
 <?php
-/*
- * @version $Id: billstate.form.php 651 2012-09-14 08:22:06Z walid $
- LICENSE
 
- This file is part of the order plugin.
-
- Order plugin is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- Order plugin is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with GLPI; along with Order. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
- @package   order
- @author    the order plugin team
- @copyright Copyright (c) 2010-2011 Order plugin team
- @license   GPLv2+
-            http://www.gnu.org/licenses/gpl.txt
- @link      https://forge.indepnet.net/projects/order
- @link      http://www.glpi-project.org/
- @since     2009
- ---------------------------------------------------------------------- */
+/**
+ * -------------------------------------------------------------------------
+ * ItilCategoryGroups plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of ItilCategoryGroups.
+ *
+ * ItilCategoryGroups is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ItilCategoryGroups is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ItilCategoryGroups. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2012-2022 by ItilCategoryGroups plugin team.
+ * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/pluginsGLPI/itilcategorygroups
+ * -------------------------------------------------------------------------
+ */
 
 include ("../../../inc/includes.php");
 
--- inc/category.class.php
+++ inc/category.class.php
@@ -1,32 +1,32 @@
 <?php
-/*
- * @version $Id: setup.php 19 2012-06-27 09:19:05Z walid $
- LICENSE
 
-  This file is part of the itilcategorygroups plugin.
-
- Order plugin is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- Order plugin is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with GLPI; along with itilcategorygroups. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
- @package   itilcategorygroups
- @author    the itilcategorygroups plugin team
- @copyright Copyright (c) 2010-2011 itilcategorygroups plugin team
- @license   GPLv2+
-            http://www.gnu.org/licenses/gpl.txt
- @link      https://forge.indepnet.net/projects/itilcategorygroups
- @link      http://www.glpi-project.org/
- @since     2009
- ---------------------------------------------------------------------- */
+/**
+ * -------------------------------------------------------------------------
+ * ItilCategoryGroups plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of ItilCategoryGroups.
+ *
+ * ItilCategoryGroups is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ItilCategoryGroups is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ItilCategoryGroups. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2012-2022 by ItilCategoryGroups plugin team.
+ * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/pluginsGLPI/itilcategorygroups
+ * -------------------------------------------------------------------------
+ */
 
 class PluginItilcategorygroupsCategory extends CommonDropdown {
 
