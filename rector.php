dc6ca68375bbea14afafd075426e7e1b52596dbd
Stanislas Kita <skita@teclib.com>
2025-09-30 15:17:24 +0200

    fix licence

--- rector.php
+++ rector.php
@@ -2,28 +2,30 @@
 
 /**
  * -------------------------------------------------------------------------
- * Metabase plugin for GLPI
+ * ItilCategoryGroups plugin for GLPI
  * -------------------------------------------------------------------------
  *
  * LICENSE
  *
- * This file is part of Metabase.
+ * This file is part of ItilCategoryGroups.
  *
- * Metabase is free software; you can redistribute it and/or modify
+ * ItilCategoryGroups is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Metabase is distributed in the hope that it will be useful,
+ * ItilCategoryGroups is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Metabase. If not, see <http://www.gnu.org/licenses/>.
+ * along with ItilCategoryGroups. If not, see <http://www.gnu.org/licenses/>.
  * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2012-2022 by ItilCategoryGroups plugin team.
  * @copyright Copyright (C) 2018-2023 by Metabase plugin team.
  * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/pluginsGLPI/itilcategorygroups
  * @link      https://github.com/pluginsGLPI/metabase
  * -------------------------------------------------------------------------
  */
